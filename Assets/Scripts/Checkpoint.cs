5713412ddc3cb460c761ed025e7b8c168f7f7404
Kevin Kulendran <kulendrp@sheridancollege.ca>
2021-04-15 17:32:03 -0400

    Added Potenital Working Check Points
    Build runs fine, check points do not work as of now

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private GameMaster gm;
+    //private GameObject gm;
+    //private GameObject gm;
+    void Start()
+    {
+       // gm = GameObject.FindGameObjectsWithTag("GM");
+        // gm = GameMaster.instance.lastCheckPointPos;
+        //gm = GameObject.Find("Game Master");
+        gm = GameObject.Find("Game Master").GetComponent<GameMaster>();
+    }
+
+    void onTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gm.lastCheckPointPos = transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerPos.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPos : MonoBehaviour{
+
+    private GameMaster gm;
+    //private GameObject gm;
+
+
+    void Start() {
+        gm = GameObject.Find("Game Master").GetComponent<GameMaster>();
+        //gm = GameObject.FindGameObjectsWithTag("GM");
+            //.<GameMaster>();
+        transform.position = gm.lastCheckPointPos;
+        
+    }
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Space)){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
